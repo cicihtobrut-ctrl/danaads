e0b34d348086e659169e0e89f184bff15321aa4c
abdidigitaldotid <abdidigitaldotid@gmail.com>
2025-09-26 12:37:31 +0700

    featired mn

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
-import { SupabaseProvider } from '../lib/SupabaseProvider'; // Import provider baru
+import dynamic from 'next/dynamic';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
+
+// Memuat SupabaseProvider secara dinamis HANYA di sisi browser
+const SupabaseProvider = dynamic(
+  () => import('../lib/SupabaseProvider').then((mod) => mod.SupabaseProvider),
+  { ssr: false } // Ini adalah bagian terpenting: NONAKTIFKAN Server-Side Rendering
+);
 
 function MyApp({ Component, pageProps }) {
   return (
